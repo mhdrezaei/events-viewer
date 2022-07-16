12ac3eb2dc08093a12725396d24f62590e8f37fb
mohammad rezaei <98229394+mhdrezaei@users.noreply.github.com>
2022-07-16 19:18:09 +0430

    filter events page

--- components/events/filter-events.js
+++ components/events/filter-events.js
@@ -1,37 +1,44 @@
-import classes from "./filter-events.module.css"
-function FilterEvents(){
+import classes from "./filter-events.module.css";
+import { useRef } from "react";
+function FilterEvents(props) {
+  const inputYearRef = useRef();
+  const inputMonthRef = useRef();
+  const submitHandle = (event) => {
+    event.preventDefault();
+    const year = inputYearRef.current.value;
+    const month = inputMonthRef.current.value;
 
-    return(
-        
-            <div className={classes.container} >
-                <div className={classes.form} >
-                    <form>
-                        <label htmlFor="year" >Year</label>
-                        <select className={classes.select} id="year" >
-                            <option value="2021" >2021</option>
-                            <option value="2022" >2022</option>
-                        </select>
-                        <label htmlFor="month">month</label>
-                        <select className={classes.select} id="month" >
-                            <option value="1" >January</option>
-                            <option value="2" >February</option>
-                            <option value="3" >March</option>
-                            <option value="4" >April</option>
-                            <option value="5" >May</option>
-                            <option value="6" >June</option>
-                            <option value="7" >July</option>
-                            <option value="8" >August</option>
-                            <option value="9" >September</option>
-                            <option value="10" >October</option>
-                            <option value="11" >November</option>
-                            <option value="12" >December</option>
-                        </select>
-                        <button className={classes.btn}>Apply</button>
-                    </form>
-                </div>
-            </div>
-      
-    );
+    props.applyFillter(year, month);
+  };
+  return (
+    <div className={classes.container}>
+      <div className={classes.form}>
+        <form onSubmit={submitHandle}>
+          <label htmlFor="year">Year</label>
+          <select ref={inputYearRef} className={classes.select} id="year">
+            <option value="2021">2021</option>
+            <option value="2022">2022</option>
+          </select>
+          <label htmlFor="month">month</label>
+          <select ref={inputMonthRef} className={classes.select} id="month">
+            <option value="1">January</option>
+            <option value="2">February</option>
+            <option value="3">March</option>
+            <option value="4">April</option>
+            <option value="5">May</option>
+            <option value="6">June</option>
+            <option value="7">July</option>
+            <option value="8">August</option>
+            <option value="9">September</option>
+            <option value="10">October</option>
+            <option value="11">November</option>
+            <option value="12">December</option>
+          </select>
+          <button className={classes.btn}>Apply</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default FilterEvents;
--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,7 +1,43 @@
-function FilteredEventsPage(){
-    return <div>
-        <h1>filterd events page</h1>
+import { useRouter } from "next/router";
+import EventList from "../../components/events/event-list";
+import { getFilteredEvents } from "../../dummy-data";
+function FilteredEventsPage() {
+  const router = useRouter();
+  const filter = router.query.slug;
+
+  if (!filter) {
+    return <p className="center">Loading</p>;
+  }
+  const numYear = +filter[0];
+  const nummonth = +filter[1];
+
+  if (
+    isNaN(numYear) ||
+    isNaN(nummonth) ||
+    numYear > 2030 ||
+    numYear < 2020 ||
+    nummonth < 1 ||
+    nummonth > 12
+  ) {
+    return <p className="center">please Enter valid filter</p>;
+  }
+
+  const matchEvents = getFilteredEvents({
+    year: numYear,
+    month: nummonth,
+  });
+
+  if(!matchEvents || matchEvents.length === 0){
+    return <p className="center" >no Event found for chosen filters</p>
+  }
+
+  console.log(matchEvents);
+
+  return (
+    <div>
+      <EventList items={matchEvents} />
     </div>
+  );
 }
 
 export default FilteredEventsPage;
